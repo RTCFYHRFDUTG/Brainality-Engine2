eeab7dd01f9a15435f314f8511fbcf8661b26db8
Brainy0789 <bpurdy12@icloud.com>
2025-10-11 13:56:45 -0700

    OH YEAH

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -5,13 +5,13 @@ import sys.FileSystem;
 
 class Paths
 {
-    public static function getSong(songName:String, type:String = 'Inst', ext:String=".mp3"):String 
+    public static function getSong(songName:String, type:String = 'Inst', ext:String="ogg"):String 
     {
-        var v:String = 'assets/songs/${formatSong(songName)}/${type}${ext}';
+        var v:String = 'assets/songs/${formatSong(songName)}/${type}.${ext}';
         if (!FileSystem.exists(v))
         {
             trace('Song does not exists!');
-            v = 'assets/sounds/cancelMenu' + ext;
+            v = 'assets/sounds/cancelMenu.' + ext;
         }
 
         trace('Getting song from ${v}');
