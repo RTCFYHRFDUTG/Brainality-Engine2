1bfe125116e9acee0b4cbaed5cbd978fc726be5f
Brainy0789 <bpurdy12@icloud.com>
2025-10-12 16:11:52 -0700

    lets try it this time

--- docs/setup/windows.bat
+++ docs/setup/windows.bat
@@ -6,9 +6,9 @@ haxelib install flixel-addons --skip-dependencies
 haxelib install hscript
 haxelib install hscript-iris
 haxelib install polymod
-haxelib git thx.core github.com/FunkinCrew/thx.core.git
+haxelib git thx.core https://github.com/FunkinCrew/thx.core
 haxelib set thx.core git
-haxelib git thx.semver github.com/FunkinCrew/thx.semver.git
+haxelib git thx.semver https://github.com/FunkinCrew/semver.thx.core
 haxelib set thx.semver git
 haxelib install hxvlc
 haxelib install hxdiscord_rpc
