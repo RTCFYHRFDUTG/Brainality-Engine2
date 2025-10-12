6649e3f1a630d50516ae28233a6bbbe563c91731
Brainy0789 <bpurdy12@icloud.com>
2025-10-12 16:01:17 -0700

    oh yeah wrong thx versions oops

--- docs/setup/windows.bat
+++ docs/setup/windows.bat
@@ -6,6 +6,10 @@ haxelib install flixel-addons --skip-dependencies
 haxelib install hscript
 haxelib install hscript-iris
 haxelib install polymod
+haxelib git thx.core github.com/FunkinCrew/thx.core.git
+haxelib set thx.core git
+haxelib git thx.semver github.com/FunkinCrew/thx.semver.git
+haxelib set thx.semver git
 haxelib install hxvlc
 haxelib install hxdiscord_rpc
 echo Finished!
