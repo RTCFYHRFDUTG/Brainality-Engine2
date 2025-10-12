21abb9139517805440869afe75815386abda6ce0
Brainy0789 <bpurdy12@icloud.com>
2025-10-12 15:50:36 -0700

    Update windows.bat

---
+++ docs/setup/windows.bat
@@ -0,0 +1,10 @@
+echo This may take a few minutes, depending on your internet speed.
+haxelib install flixel 6.1.0
+haxelib set flixel 6.1.0
+haxelib install flixel-ui --skip-dependencies
+haxelib install flixel-addons --skip-dependencies
+haxelib install hscript-iris
+haxelib install polymod
+haxelib install hxvlc
+haxelib install hxdiscord_rpc
+echo Finished!
