4f076a26428e2cf1cc03142aca00dca063a0a8e6
RTCFYHRFDUTG <JAIDJAIDEN94@GMAIL.COM>
2025-10-20 17:21:59 -0700

    Add mac.sh for Haxe library setup
    This script installs various Haxe libraries and sets their versions.

---
+++ docs/setup/mac.sh
@@ -0,0 +1,12 @@
+echo This may take a few minutes, depending on your internet speed.
+haxelib install flixel 6.1.0
+haxelib set flixel 6.1.0
+haxelib install flixel-ui --skip-dependencies
+haxelib install flixel-addons --skip-dependencies
+haxelib install hscript-iris
+haxelib git polymod https://github.com/BrainalityTeam/polymod-brainality.git
+haxelib set polymod git
+haxelib install hxdiscord_rpc 1.3.0
+haxelib set hxdiscord_rpc 1.3.0
+haxelib install hxvlc
+echo Finished!
